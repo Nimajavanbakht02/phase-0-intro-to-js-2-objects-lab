e2128daedf54d1674f0db2815124965c67ee7741
NimaJavanbakht <nimabrah19@gmail.com>
2024-01-30 12:10:58 -0800

    modifiesobjects

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+        name: 'Nima',
+        streetaddress: '15 MammothAvenue',
+    }
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let copyOfEmployee = {...employee}
+    copyOfEmployee[key] = value
+    return copyOfEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let copyOfEmployee = {...employee}
+    delete copyOfEmployee[key]
+    return copyOfEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
